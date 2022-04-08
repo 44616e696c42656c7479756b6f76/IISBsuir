f477e0ed50447a26012cfea3566d3cd56c4c79e9
beltyukov.e.danil@gmail.com <beltyukov.e.danil@gmail.com>
2022-04-09 01:03:14 +0300

    Create client with authorization

---
+++ IISHelper/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISHelper
+{
+    public class Client
+    {
+        private readonly HttpClient _HttpClient;
+
+        public Client()
+        {
+            _HttpClient = new HttpClient();
+        }
+
+        public static async Task<(HttpResponseMessage, Cookie)> TryAuth(string UserName, string Password)
+        {
+            var query = "https://iis.bsuir.by/api/v1/auth/login";
+
+            try
+            {
+                var cookies = new CookieContainer();
+                var httpClient = new HttpClient(new HttpClientHandler { CookieContainer = cookies });
+                var stringContent = new StringContent($"{{\"username\": \"{UserName}\", \"password\": \"{Password}\"}}", Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync(query, stringContent);
+                var responseCookies = cookies.GetCookies(new Uri(query)).Cast<Cookie>();
+
+                return (response, responseCookies.First());
+            }
+            catch (Exception ex)
+            {
+                // Logginig
+            }
+
+            return (null, null);
+        }
+    }
+}
