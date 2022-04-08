57e3ecf9e18fefb456b8b49e94cf9b57af5f443e
beltyukov.e.danil@gmail.com <beltyukov.e.danil@gmail.com>
2022-04-09 01:31:57 +0300

    Create model 'PersonalCv'. Get it by http.

--- IISHelper/Client.cs
+++ IISHelper/Client.cs
@@ -1,22 +1,50 @@
-﻿using System;
+﻿using IISHelper.Models;
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IISHelper
 {
-    public class Client
-    {
-        private readonly HttpClient _HttpClient;
+    public class Client 
+    {   
+        public string Username { get; private set; }
+        public bool IsAuthorized { get; private set; } = false;
 
-        public Client()
+        private readonly HttpClient _HttpClient;
+        private readonly CookieContainer _CookieContainer;
+        private const string _BaseAddress = "https://iis.bsuir.by/api/v1/";
+
+        public Client(string Username, string Password, bool LogIn = true)
         {
-            _HttpClient = new HttpClient();
+            _CookieContainer = new CookieContainer();
+            _HttpClient = new HttpClient(new HttpClientHandler { CookieContainer = _CookieContainer })
+            {
+                BaseAddress = new Uri(_BaseAddress)
+            };
+
+            this.Username = Username;
+
+            if (LogIn)
+            {
+                var authResult = TryAuth(Username, Password).Result;
+                if (authResult.Item2 != null)
+                {
+                    _CookieContainer.Add(authResult.Item2);
+                    IsAuthorized = true;
+                }
+            }
         }
 
-        public static async Task<(HttpResponseMessage, Cookie)> TryAuth(string UserName, string Password)
+        public async Task<PersonalCv> GetPersonalCv()
+        {
+            return await _HttpClient.GetFromJsonAsync<PersonalCv>("profiles/personal-cv");
+        }
+
+        public static async Task<(HttpResponseMessage, Cookie)> TryAuth(string Username, string Password)
         {
             var query = "https://iis.bsuir.by/api/v1/auth/login";
 
@@ -24,7 +52,7 @@ namespace IISHelper
             {
                 var cookies = new CookieContainer();
                 var httpClient = new HttpClient(new HttpClientHandler { CookieContainer = cookies });
-                var stringContent = new StringContent($"{{\"username\": \"{UserName}\", \"password\": \"{Password}\"}}", Encoding.UTF8, "application/json");
+                var stringContent = new StringContent($"{{\"username\": \"{Username}\", \"password\": \"{Password}\"}}", Encoding.UTF8, "application/json");
 
                 var response = await httpClient.PostAsync(query, stringContent);
                 var responseCookies = cookies.GetCookies(new Uri(query)).Cast<Cookie>();
@@ -33,7 +61,8 @@ namespace IISHelper
             }
             catch (Exception ex)
             {
-                // Logginig
+                // Loggining
+                throw;
             }
 
             return (null, null);
---
+++ IISHelper/Models/PersonalCv.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace IISHelper.Models
+{
+    public class PersonalCv
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("middleName")]
+        public string MiddleName { get; set; }
+
+        [JsonProperty("birthDate")]
+        public string BirthDate { get; set; }
+
+        [JsonProperty("photoUrl")]
+        public Uri PhotoUrl { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("rating")]
+        public long Rating { get; set; }
+
+        [JsonProperty("showRating")]
+        public bool ShowRating { get; set; }
+
+        [JsonProperty("published")]
+        public bool Published { get; set; }
+
+        [JsonProperty("searchJob")]
+        public bool SearchJob { get; set; }
+
+        [JsonProperty("faculty")]
+        public string Faculty { get; set; }
+
+        [JsonProperty("course")]
+        public long Course { get; set; }
+
+        [JsonProperty("officeEmail")]
+        public string OfficeEmail { get; set; }
+
+        [JsonProperty("officePassword")]
+        public string OfficePassword { get; set; }
+
+        [JsonProperty("speciality")]
+        public string Speciality { get; set; }
+
+        [JsonProperty("studentGroup")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long StudentGroup { get; set; }
+
+        [JsonProperty("skills")]
+        public Skill[] Skills { get; set; }
+
+        [JsonProperty("references")]
+        public object[] References { get; set; }
+    }
+
+    public partial class Skill
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+    }
+}
